66870ae00e83f0f846dc238d278ddc257cdfdc0d
indhucs <indhu.cs@gmail.com>
2016-09-21 21:00:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-1.html', function(req,res) {
+    res.sendFile(path.join(__dirname,'article-1.html'))
+})
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
