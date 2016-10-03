7bff3b84a148280f7da90c73332b4762da2d7e5c
indhucs <indhu.cs@gmail.com>
2016-10-03 17:27:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,6 @@ var button = document.getElementById("counter");
 button.onclick = function() {
     var request = new XMLHttpRequest();
     
-    request.open("GET","http://indhucs.imad.hasura-app.io/counter",true);
-    request.send(null);
-    
     request.onreadystatechange = function() {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.Status === 200) {
@@ -15,4 +12,8 @@ button.onclick = function() {
             }
         }
     };
+    
+    request.open("GET","http://indhucs.imad.hasura-app.io/counter",true);
+    request.send(null);
+    
 };
