664f09a59c3bc26e060f40b89b41bb71a97a3863
indhucs <indhu.cs@gmail.com>
2016-10-03 17:22:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
-console.log('Loaded!');
+var button = document.getElementById("counter");
+button.onclick = function() {
+    var request = new XMLHttpRequest();
+    
+    request.open("GET","http://indhucs.imad.hasura-app.io/counter",true);
+    request.send(null);
+    
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.Status === 200) {
+                var counter = request.resposnseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+};
